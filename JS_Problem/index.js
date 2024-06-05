275e43b476423ac5bb7105fb75b56c75e3ec127e
Samkit Saraf <63035142+samkit09@users.noreply.github.com>
2024-06-04 23:07:22 -0500

    init

---
+++ JS_Problem/index.js
@@ -0,0 +1,28 @@
+// Q1. Concatenate the two arrow
+const names = [
+    { userid: 2, name: "Velen" },
+    { userid: 56, name: "Illidan" },
+    { userid: 23, name: "Muradin" },
+    { userid: 12, name: "Sylvanas" },
+    { userid: 44, name: "Cenarius" },
+    { userid: 4, name: "Gul'Dan" },
+  ];
+  const roles = [
+    { userid: 2, role: "Mage" },
+    { userid: 4, role: "Worlock" },
+    { userid: 56, role: "Demon Hunter" },
+    { userid: 66, role: "Druid" },
+    { userid: 87, role: "Shaman" },
+    { userid: 12, role: "Hunter" },
+  ];
+
+// 2. 
+
+const callback1 = (a) => a + 2; // 6
+const callback2 = (b) => b * 2; // 12
+const callback3 = (c) => c - 2; // 10
+console.log(runAll(4)(callback1, callback2, callback3)); // 10
+function runAll(initNum) {
+  return function(...args) {
+  }
+}
