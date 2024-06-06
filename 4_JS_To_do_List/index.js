be26f3912afbe902f89bf3c136db5ef1f7263c9b
Samkit Saraf <63035142+samkit09@users.noreply.github.com>
2024-06-06 11:40:40 -0500

    fin

--- 4_JS_To_do_List/index.js
+++ 4_JS_To_do_List/index.js
@@ -1,5 +1,45 @@
 let todos = fetch("https://jsonplaceholder.typicode.com/todos");
-
+var frag = document.createDocumentFragment();
 todos.then(res => res.json())
-  .then(d => console.log(d));
+  .then(d => {
+    d.forEach(({title, completed}) => {
+      const li = document.createElement("li");
+      li.textContent = title;
+      if (completed){
+        li.classList.add("checked");
+      }
+      li.appendChild(createClose());
+      frag.appendChild(li);
+    });
+    document.getElementById("list").appendChild(frag);
+  }).catch((err) => alert("Error: ",err));
 
+function createClose(){
+  const span = document.createElement("span");
+  span.className = "close";
+  span.textContent = "\u00D7";
+  span.onclick = function () {
+    this.parentElement.style.display = "none";
+  };
+  return span
+}
+
+function addElem(){
+  let title = document.getElementById("inp").value;
+  console.log("Adding element", title);
+  if (title.length > 0){
+    const li = document.createElement("li");
+    li.textContent = title;
+    li.appendChild(createClose());
+    frag.appendChild(li);
+    document.getElementById("list").prepend(frag);
+    console.log("Added");
+  }
+};
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(task) {
+  if (task.target.tagName === 'LI') {
+    task.target.classList.toggle('checked');
+  }
+}, false);
